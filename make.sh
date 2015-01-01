cdb57245a1b57825881dc251a74cbdd7eb1181bf
Tomoki Imai <tomo832@gmail.com>
2015-01-02 00:32:49 +0900

    rename library.html to index.html

--- make.sh
+++ make.sh
@@ -10,4 +10,4 @@ cd source
 echo "making with ${files[*]}"
 # this make truly standalone html.MathML is not working in chrome 26 now.
 pandoc ${files[*]} -c library.css -t html5 -s --toc --mathjax --highlight-style tango  \
-    -f markdown+hard_line_breaks -N --filter ../include_file.hs --filter ../yasnippet.hs -o ../library.html
+    -f markdown+hard_line_breaks -N --filter ../include_file.hs --filter ../yasnippet.hs -o ../index.html
